e288dc7f429860391241f68ecf13687a8f7352cf
nghipq <nghipqce140179@fpt.edu.vn>
2019-07-17 23:18:38 +0700

    change page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AuthProvider from './contexts/AuthProvider';
 import { CartContext } from './contexts/CartContext';
 import Nav from './components/navbar';
+import Home from './pages/Home';
 import AuthLogin from './pages/Auth/AuthLogin';
 import AuthRegister from './pages/Auth/AuthRegister';
 import ListProducts from './components/ListProducts';
-import Order from './pages/Order';
 import Cart from './pages/Cart';
 
 function App() {
@@ -22,11 +22,11 @@ function App() {
           <div className="App">
             <Nav/>
           </div>
-          <Route path="/" exact component={ListProducts} />
+          <Route path="/" exact component={Home} />
+          <Route path="/dat-hang" exact component={ListProducts} />
+          <Route path="/gio-hang" exact component={Cart} />
           <Route path="/auth/login" exact component={AuthLogin} />
           <Route path="/auth/register" exact component={AuthRegister} />
-          <Route path="/dat-hang" exact component={Order} />
-          <Route path="/gio-hang" exact component={Cart} />
         </Router>
       </CartContext>
     </AuthProvider>
